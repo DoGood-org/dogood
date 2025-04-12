8cf1ed8a71bb8afb237098619fa5110cbb6ecef9
BondarenkoDoGood <dogood3777@gmail.com>
2025-04-12 13:05:03 +0200

    Update Layout.js

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,10 +1,7 @@
-import Sidebar from "./Sidebar";
-
 export default function Layout({ children }) {
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="ml-60 w-full p-4">{children}</main>
-    </div>
+    <main className="w-full p-4">
+      {children}
+    </main>
   );
 }
