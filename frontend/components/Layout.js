5fb7cd2fb345a69a6b1c07c0b75764d455ab9789
BondarenkoDoGood <dogood3777@gmail.com>
2025-04-12 18:17:24 +0200

    Update Layout.js

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,10 +1,12 @@
-// components/Layout.js
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col bg-white text-gray-900">
-      <main className="flex-1">
-        {children}
-      </main>
+      <NavBar />
+      <main className="flex-1">{children}</main>
+      <Footer />
     </div>
   );
 }
