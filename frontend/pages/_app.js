9941a522ba785efec68c53da38ea718933eba78b
BondarenkoDoGood <dogood3777@gmail.com>
2025-04-12 13:25:36 +0200

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,5 @@
 // pages/_app.js
 import NavBar from "@/components/NavBar";
-import Sidebar from "@/components/Sidebar";
 import Footer from "@/components/Footer";
 import "@/styles/globals.css";
 import Script from "next/script";
@@ -8,53 +7,48 @@ import Script from "next/script";
 // Google Analytics Measurement ID
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
-// App wrapper with sidebar, navbar and footer layout
+// App wrapper with navbar and footer layout
 function MyApp({ Component, pageProps }) {
-    return (
+  return (
+    <>
+      {/* Google Analytics integration */}
+      {GA_ID && (
         <>
-            {/* Google Analytics integration */}
-            {GA_ID && (
-                <>
-                    <Script
-                        strategy="afterInteractive"
-                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-                    />
-                    <Script
-                        id="google-analytics"
-                        strategy="afterInteractive"
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', '${GA_ID}', {
-                                    page_path: window.location.pathname,
-                                });
-                            `,
-                        }}
-                    />
-                </>
-            )}
-
-            <div className="flex bg-gray-950 text-white min-h-screen">
-                {/* Sidebar on the left */}
-                <Sidebar />
-
-                {/* Main content area */}
-                <div className="flex flex-col flex-1 ml-60">
-                    <NavBar />
-
-                    {/* Page-specific content */}
-                    <main className="flex-1 p-4">
-                        <Component {...pageProps} />
-                    </main>
-
-                    {/* Footer */}
-                    <Footer />
-                </div>
-            </div>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
         </>
-    );
+      )}
+
+      <div className="flex flex-col bg-gray-950 text-white min-h-screen">
+        {/* Navigation bar at the top */}
+        <NavBar />
+
+        {/* Main content area */}
+        <main className="flex-1 p-4">
+          <Component {...pageProps} />
+        </main>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default MyApp;
