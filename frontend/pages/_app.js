25cecccb4f816c5ee35ef600e6920b739c1fe7b1
BondarenkoDoGood <dogood3777@gmail.com>
2025-04-12 18:05:23 +0200

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,14 @@
 // pages/_app.js
-import NavBar from "@/components/NavBar";
-import Footer from "@/components/Footer";
+import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import Script from "next/script";
 
-// Google Analytics Measurement ID
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
-// App wrapper with navbar and footer layout
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* Google Analytics integration */}
+      {/* Google Analytics */}
       {GA_ID && (
         <>
           <Script
@@ -35,18 +32,10 @@ function MyApp({ Component, pageProps }) {
         </>
       )}
 
-      <div className="flex flex-col bg-gray-950 text-white min-h-screen">
-        {/* Navigation bar at the top */}
-        <NavBar />
-
-        {/* Main content area */}
-        <main className="flex-1 p-4">
-          <Component {...pageProps} />
-        </main>
-
-        {/* Footer */}
-        <Footer />
-      </div>
+      {/* Wrapper layout */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
